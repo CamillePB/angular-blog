03fedc93dd396f1a8b097b911ebf7ae7692652c4
CamillePB <camille.pedro.bueno@hotmail.com>
2023-09-19 13:45:20 -0300

    cards dinamicos

--- src/app/components/cards/cards.component.ts
+++ src/app/components/cards/cards.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { cardsData } from 'src/app/data/cardsData';
 
 @Component({
   selector: 'app-cards',
   templateUrl: './cards.component.html',
   styleUrls: ['./cards.component.css']
 })
-export class CardsComponent {
+export class CardsComponent implements OnInit {
 
+  @Input()
+  photoCover:string = ""
+
+  @Input()
+  cardTitle:string = ""
+
+  @Input()
+  cardSubtitle:string = ""
+
+  @Input()
+  Id:string="0"
+
+  constructor() {
+   }
+
+  ngOnInit(): void {
+  }
 }
---
+++ src/app/data/cardsData.ts
@@ -0,0 +1,38 @@
+export const cardsData = [
+  {
+    "id":"1",
+    "cardTitle": "Clean & Simple",
+    "cardSubtitle": "A clean interfaces is the key to a good looking product. We always like to keep things clean and simple.",
+    "photoCover":"../../../assets/checked.png"
+  },
+  {
+    "id":"2",
+    "title": "Pixel Perfect",
+    "description": "Every pixel, icon or piece of interface on this awesome theme is manually and carefully crafted to perfection.",
+    "photoCover":"../../../assets/square.png"
+  },
+  {
+    "id":"3",
+    "title": "Multiple Options",
+    "description": "The theme comes eith a variety of colors, styles and layouts, so it can be match for any business.",
+    "photoCover":"../../../assets/archive.png"
+  },
+  {
+    "id":"4",
+    "title": "Retina Ready",
+    "description": "The theme was designed and developed to look absoutely amazinf on any retina resolution.",
+    "photoCover":"../../../assets/desktop.png"
+  },
+  {
+    "id":"5",
+    "title": "Responsive",
+    "description": "Your clients will be able to have a unique, awesome and smart experience on every mobile device out there.",
+    "photoCover":"../../../assets/responsive.png"
+  },
+  {
+    "id":"6",
+    "title": "Sketch Support",
+    "description": "The design was created in Photoshop and Sketch, so you'll be able to use your favorite software.",
+    "photoCover":"../../../assets/diamond.png"
+  }
+]
